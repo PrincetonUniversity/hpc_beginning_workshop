b2e2b6fc519aae8223da0deaec4474c0b1d53cbe
Jonathan Halverson <52128661+jdh4@users.noreply.github.com>
2025-10-14 11:19:09 -0400

    Update send_recv.py

--- mpi4py/solution/send_recv.py
+++ mpi4py/solution/send_recv.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     if rank == 0:
         data = 42
         comm.send(data, dest=1, tag=12345)
+        print(f"Process 0 sent the value {data} to process 1")
     elif rank == 1:
         data = comm.recv(source=0, tag=12345)
-        print(f"Process 1 received the value {data}")
+        print(f"Process 1 received the value {data} from process 0")
